5a0f510ad573b4562294b72830d679c5f490cc75
Tetiana Pyatachuk <pyatachuk26@gmail.com>
2026-01-31 17:20:24 +0200

    sum

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,12 @@
-// export const Sum = () => ();
+export const Sum = ({ a = 0, b = 0 }) => {
+
+  const x = Number(a) || 0;
+  const y = Number(b) || 0;
+
+  return (
+    <p>
+      Sum of {x} and {y} is {x + y}
+    </p>
+  );
+
+};
